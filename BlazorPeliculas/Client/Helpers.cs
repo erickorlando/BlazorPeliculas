7a3cdafead4f2f4d269a95e7347beed06fdfca1c
Erick Orlando <erick.velasco@gotocme.com>
2023-05-06 19:01:11 -0500

    Interoperabilidad de JS

---
+++ BlazorPeliculas/Client/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorPeliculas.Client;
+
+public static class Helpers
+{
+    public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+    {
+        // Cuando quiero ejecutar un metodo de JS sin retorno de datos
+
+        await js.InvokeVoidAsync("console.log", "Prueba de Consola desde Blazor WASM");
+
+        // Cuando requiero un valor de retorno de JS
+        return await js.InvokeAsync<bool>("confirm", message);
+    }
+}
